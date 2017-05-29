322ed29d585ad2791ad20098d9add3a0efe0ff93
Piergiuseppe Isonni <p.isonni@outlook.com>
2017-05-29 23:08:30 +0200

    limit 10 elements in foodpreference

--- app/routers/food_router.js
+++ app/routers/food_router.js
@@ -73,7 +73,7 @@ router.route('/foods/:food_id').get(function(req, res){
 
 router.route('/foodpreference').get(function(req, res){
 	if (req.query.mail) {
-		Food.findRandom({user_preference: {$nin: [req.query.mail]}}).limit(20).exec(function(err, foods){
+		Food.findRandom({user_preference: {$nin: [req.query.mail]}}).limit(10).exec(function(err, foods){
 			if (err) {
 				console.log("ciao");
 				res.send(err);
