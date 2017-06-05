1e0d17b6210c20b98c104ae54e06b0b635f1db6f
Piergiuseppe Isonni <p.isonni@outlook.com>
2017-06-05 21:41:39 +0200

    shopping list fixed

--- app/routers/shopping_router.js
+++ app/routers/shopping_router.js
@@ -12,129 +12,179 @@ var User = require('../models/user');
 var router = express.Router();
 
 // get ShoppingList
-router.route('/shoppinglist').get(function(req,res){
-	if (req.query.mail) {
-		User.findOne({mail: req.query.mail}).exec(function(err, user){
-			if (err) {
-				res.send(err);
-			} else {
-				ShoppingList.findOne({user: user._id, completed: false}, {}, { sort: { 'created_at' : -1 } }).populate('items').exec(function(err,list){
-					if (err) {
-						res.send(err);
-					} else {
-						res.json(list);
-					}
-				});
-			}
-		});
-	} else {
-		res.json({
-				message: "Cannot modify user without identifier"
-		});
-	}
+router.route('/shoppinglist').get(function(req, res) {
+  if (req.query.mail) {
+    User.findOne({
+      mail: req.query.mail
+    }).exec(function(err, user) {
+      if (err) {
+        res.send(err);
+      } else {
+        ShoppingList.findOne({
+          user: user._id,
+          completed: false
+        }, {}, {
+          sort: {
+            'created_at': -1
+          }
+        }).populate('items').exec(function(err, list) {
+          if (err) {
+            res.send(err);
+          } else {
+            res.json(list);
+          }
+        });
+      }
+    });
+  } else {
+    res.json({
+      message: "Cannot modify user without identifier"
+    });
+  }
 });
 
 // post ShoppingList
-router.route('/shoppinglist').post(function(req,res){
-	if (req.body.mail) {
-		User.findOne({mail: req.body.mail}).exec(function(err, user){
-			if (err) {
-				res.send(err);
-			} else {
-				var shoppingList = new ShoppingList();
-				shoppingList.user = user;
-				shoppingList.title = req.body.title;
-				shoppingList.items = [];
-				for (i = 0; i < req.body.items.length; i++) {
-					var shoppingItem = new ShoppingItem();
-					shoppingItem.name = req.body.items[i].name;
-					if (req.body.items[i].value) {
-						shoppingItem.value = req.body.items[i].value;
-					}
-					if (req.body.items[i].unit) {
-						shoppingItem.unit = req.body.items[i].unit;
-					}
-					shoppingItem.save();
-					shoppingList.items.push(shoppingItem);
-				}
-				shoppingList.save(function(err){
-					if (err) {
-						res.send(err);
-					} else {
-						res.json({status: 200, message: "OK"});
-					}
-				});
-			}
-		});
-	} else {
-		res.json({
-				message: "Cannot modify user without identifier"
-		});
-	}
+router.route('/shoppinglist').post(function(req, res) {
+  if (req.body.mail) {
+    User.findOne({
+      mail: req.body.mail
+    }).exec(function(err, user) {
+      if (err) {
+        res.send(err);
+      } else {
+        var shoppingList = new ShoppingList();
+        shoppingList.user = user._id;
+        shoppingList.title = req.body.title;
+        shoppingList.items = [];
+        for (i = 0; i < req.body.items.length; i++) {
+          var shoppingItem = new ShoppingItem();
+          shoppingItem.name = req.body.items[i].name;
+          if (req.body.items[i].value) {
+            shoppingItem.value = req.body.items[i].value;
+          }
+          if (req.body.items[i].unit) {
+            shoppingItem.unit = req.body.items[i].unit;
+          }
+          shoppingItem.save();
+          shoppingList.items.push(shoppingItem);
+        }
+        shoppingList.save(function(err) {
+          if (err) {
+            res.send(err);
+          } else {
+            res.json({
+              status: 200,
+              message: "OK"
+            });
+          }
+        });
+      }
+    });
+  } else {
+    res.json({
+      message: "Cannot modify user without identifier"
+    });
+  }
 });
 
 // complete list
-router.route('/listcomplete').post(function(req,res){
-	ShoppingList.findById(req.body.list_id).exec(function(err, list){
-		if (err) {
-			res.send(err);
-		} else {
-			list.completed = true;
-			list.completed_at = new Date();
-			list.save(function(err){
-				if (err) {
-					res.send(err);
-				} else {
-					res.json({status: 200, message: "OK"});
-				}
-			});
-		}
-	});
+router.route('/listcomplete').post(function(req, res) {
+  ShoppingList.findById(req.body.list_id).exec(function(err, list) {
+    if (err) {
+      res.send(err);
+    } else {
+      list.completed = true;
+      list.completed_at = new Date();
+      list.save(function(err) {
+        if (err) {
+          res.send(err);
+        } else {
+          res.json({
+            status: 200,
+            message: "OK"
+          });
+        }
+      });
+    }
+  });
 });
 
 // complete item
-router.route('/itemchecked').post(function(req,res){
-	ShoppingItem.findById(req.body.item_id).exec(function(err, item){
-		if (err) {
-			res.send(err);
-		} else {
-			item.checked = req.body.checked;
-			item.save(function(err){
-				if (err) {
-					res.send(err);
-				} else {
-					res.json(item);
-				}
-			});
-		}
-	});
+router.route('/itemchecked').post(function(req, res) {
+  ShoppingItem.findById(req.body.item_id).exec(function(err, item) {
+    if (err) {
+      res.send(err);
+    } else {
+      item.checked = req.body.checked;
+      item.save(function(err) {
+        if (err) {
+          res.send(err);
+        } else {
+          res.json(item);
+        }
+      });
+    }
+  });
 });
 
 // add item
-router.route('/additem').post(function(req,res){
-	ShoppingList.findById(req.body.list_id).exec(function(err, list){
-		if (err) {
-			res.send(err);
-		} else {
-			var item = new ShoppingItem();
-			item.name = req.body.item.name;
-			if (req.body.item.value) {
-				item.value = req.body.item.value;
-			}
-			if (req.body.item.unit) {
-				item.unit = req.body.item.unit;
-			}
-			item.save();
-			list.items.push(item);
-			list.save(function(err){
-				if (err) {
-					res.send(err);
-				} else {
-					res.json(list);
-				}
-			});
-		}
-	});
+router.route('/additem').post(function(req, res) {
+  if (req.body.list_id) {
+    ShoppingList.findById(req.body.list_id).exec(function(err, list) {
+      if (err) {
+        res.send(err);
+      } else {
+        var item = new ShoppingItem();
+        item.name = req.body.item.name;
+        if (req.body.item.value) {
+          item.value = req.body.item.value;
+        }
+        if (req.body.item.unit) {
+          item.unit = req.body.item.unit;
+        }
+        item.save();
+        list.items.push(item);
+        list.save(function(err) {
+          if (err) {
+            res.send(err);
+          } else {
+            res.json(list);
+          }
+        });
+      }
+    });
+  } else if (req.body.mail) {
+    User.findOne({
+      mail: req.body.mail
+    }).exec(function(err, user) {
+      if (err) {
+        res.send(err);
+      } else {
+        var shoppingList = new ShoppingList();
+        shoppingList.user = user._id;
+        shoppingList.title = req.body.title;
+        shoppingList.items = [];
+        var item = new ShoppingItem();
+        item.name = req.body.item.name;
+        if (req.body.item.value) {
+          item.value = req.body.item.value;
+        }
+        if (req.body.item.unit) {
+          item.unit = req.body.item.unit;
+        }
+        item.save();
+        shoppingList.items.push(item);
+        shoppingList.save(function(err) {
+          if (err) {
+            res.send(err);
+          }
+        });
+				res.json(shoppingList);
+      }
+    });
+  } else {
+		res.json({status: 400, message: "error"});
+	}
 });
 
 module.exports = router;
